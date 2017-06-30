f0cbde2a63cde4851125a965903b92362bd56052
Michael Giordano <michaelgiordano@michaels-macbook-air.local>
2017-06-30 14:25:47 -0500

    just the setup, no code yet

---
+++ App 4 - Build Your Own - Minesweeper/SecondViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SecondViewController.swift
+//  App 4 - Build Your Own - Minesweeper
+//
+//  Created by Michael Giordano on 6/30/17.
+//  Copyright © 2017 Michael Giordano. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- App 4 - Build Your Own - Minesweeper/ViewController.swift
+++ App 4 - Build Your Own - Minesweeper/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var bombs: UILabel!
+    
+    func rand(lim : Int) -> Int
+    {
+        return Int(arc4random_uniform(UInt32(lim)))
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func whenTapped(_ sender: UITapGestureRecognizer)
+    {
+        
     }
-
-
+    
+    @IBAction func onToggleTapped(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func onResetTapped(_ sender: Any)
+    {
+        
+    }
 }
 
